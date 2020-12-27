5b4f6c6ef376462f96eb8a55b0cf3ec591f5352a
yusami <usami2go@yahoo.co.jp>
2020-12-27 14:34:52 +0900

    Add the build config for nokogiri on Big Sur.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ if [ -e vendor ]; then
   echo "gems are already installed."
 else
   echo "Installing gems..."
+  bundle config build.nokogiri --use-system-libraries \
+    --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
   bundle install --path vendor/bundle --without development test
 fi
