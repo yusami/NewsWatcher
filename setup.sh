6ecf835422ae17fdcb14d9e6ede071d4d718a4b3
yusami <usami2go@yahoo.co.jp>
2021-05-09 22:02:19 +0900

    Update the project for Ruby 2.7 on M1 with BigSur.

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ if [ -e vendor ]; then
   echo "gems are already installed."
 else
   echo "Installing gems..."
-  bundle config build.nokogiri --use-system-libraries \
-    --with-xml2-include=$(brew --prefix libxml2)/include/libxml2
   bundle config set --local path 'vendor/bundle'
   bundle config set --local without 'development test'
   bundle install
